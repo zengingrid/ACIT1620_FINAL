0ffac5ca4366f877abdf1e5a2894037178392fc0
Inggy <71790092+zengingrid@users.noreply.github.com>
2020-12-09 15:43:35 -0800

    added function to submit button, blur effect working for the displayed image

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const form = document.querySelector(".form");
 const thumbBar = document.getElementById("thumb-bar");
-var displayImg = document.querySelector(".displayed-img")
 const gallary = [
-    { name: "Pic1", src: "images/pic1.jpg"},
-    { name: "Pic2", src: "images/pic2.jpg"},
-    { name: "Pic3", src: "images/pic3.jpg"},
-    { name: "Pic4", src: "images/pic4.jpg"},
-    { name: "Pic5", src: "images/pic5.jpg"},
-  ]
+    { name: "Pic1", src: "images/pic1.jpg", blur: "images/pic1B.jpg"},
+    { name: "Pic2", src: "images/pic2.jpg", blur: "images/pic2B.jpg"},
+    { name: "Pic3", src: "images/pic3.jpg", blur: "images/pic3B.jpg"},
+    { name: "Pic4", src: "images/pic4.jpg", blur: "images/pic4B.jpg"},
+    { name: "Pic5", src: "images/pic5.jpg", blur: "images/pic5B.jpg"},
+  ];
+var displayImg = document.querySelector(".displayed-img");
+var input = document.getElementById("effect");
+
 
 window.addEventListener('load', createImage(gallary));
+
 
 function createImage(gallary) {
     for (image in gallary) {
@@ -20,12 +23,31 @@ function createImage(gallary) {
     }
 };
 
+
 thumbBar.addEventListener('mouseover', function(e) {
     var displayedSrc = e.target.getAttribute("src");
-    displayImg.src = `${displayedSrc}`;;
+    displayImg.src = `${displayedSrc}`;
+    displayImg.name = e.target.getAttribute("alt")
     form.style.display = "block";
 }, capture=true);
 
+
+function submit(gallary) {
+    effectEntered = input.value;
+    if (effectEntered == "blur") {
+        effectImg = gallary.find(isImage);
+        displayImg.src = effectImg.blur;
+        input.value = null;
+    } else {
+        alert("Effect Error: wrong effect has been entered.")
+    }
+}
+
+
+function isImage(image) {
+    matchName = displayImg.name;
+    return image.name == matchName;
+}
 
 
 
